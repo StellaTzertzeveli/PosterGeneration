0a7946decc47f1cf35dfc3b558e1676d674e2aee
StellaTzertzeveli <s.i.tzertzeveli@student.utwente.nl>
2025-04-09 11:45:30 +0200

    new data

---
+++ Poster.py
@@ -0,0 +1,5 @@
+#this file turns a picture into the poster
+#get person without background, match to correct background
+#user can choose location & size of themselves and then write a title
+#then the poster is saved/ printed
+
